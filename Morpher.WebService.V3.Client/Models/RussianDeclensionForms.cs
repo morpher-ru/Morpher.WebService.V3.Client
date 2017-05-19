a0c19857b7acb0c9d82dd34bed1abb5b579e3b02
Sergey Slepov <slepov@gmail.com>
2017-05-19 14:33:41 +0100

    Format code in RussianDeclensionForms.cs.

--- Morpher.WebService.V3.Client/Models/RussianDeclensionForms.cs
+++ Morpher.WebService.V3.Client/Models/RussianDeclensionForms.cs
@@ -5,25 +5,12 @@
     [DataContract]
     public class RussianDeclensionForms
     {
-        [DataMember(Name = "И")]
-        public string Nominative { get; set; }
-
-        [DataMember(Name = "Р")]
-        public string Genitive { get; set; }
-
-        [DataMember(Name = "Д")]
-        public string Dative { get; set; }
-
-        [DataMember(Name = "В")]
-        public string Accusative { get; set; }
-
-        [DataMember(Name = "Т")]
-        public string Instrumental { get; set; }
-
-        [DataMember(Name = "П")]
-        public string Prepositional { get; set; }
-
-        [DataMember(Name = "П-о")]
-        public string PrepositionalWithO { get; set; }
+        [DataMember(Name = "И")] public string Nominative { get; set; }
+        [DataMember(Name = "Р")] public string Genitive { get; set; }
+        [DataMember(Name = "Д")] public string Dative { get; set; }
+        [DataMember(Name = "В")] public string Accusative { get; set; }
+        [DataMember(Name = "Т")] public string Instrumental { get; set; }
+        [DataMember(Name = "П")] public string Prepositional { get; set; }
+        [DataMember(Name = "П-о")] public string PrepositionalWithO { get; set; }
    }
 }
