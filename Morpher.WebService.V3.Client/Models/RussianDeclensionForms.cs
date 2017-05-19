ed44d452d65f648700030beb2be0195bafb6f9d0
Sergey Slepov <slepov@gmail.com>
2017-05-19 14:31:05 +0100

    Remove IEquatable from RussianDeclensionForms. YAGNI.

--- Morpher.WebService.V3.Client/Models/RussianDeclensionForms.cs
+++ Morpher.WebService.V3.Client/Models/RussianDeclensionForms.cs
@@ -1,11 +1,9 @@
 ﻿namespace Morpher.WebService.V3
 {
-    using System;
-    using System.Diagnostics.CodeAnalysis;
     using System.Runtime.Serialization;
 
     [DataContract]
-    public class RussianDeclensionForms : IEquatable<RussianDeclensionForms>
+    public class RussianDeclensionForms
     {
         [DataMember(Name = "И")]
         public string Nominative { get; set; }
@@ -27,57 +25,5 @@
 
         [DataMember(Name = "П-о")]
         public string PrepositionalWithO { get; set; }
-
-        [SuppressMessage("ReSharper", "StyleCop.SA1126")]
-        public static bool operator ==(RussianDeclensionForms left, RussianDeclensionForms right)
-        {
-            return Equals(left, right);
-        }
-
-        [SuppressMessage("ReSharper", "StyleCop.SA1126")]
-        public static bool operator !=(RussianDeclensionForms left, RussianDeclensionForms right)
-        {
-            return !Equals(left, right);
-        }
-
-        [SuppressMessage("ReSharper", "StyleCop.SA1503")]
-        public bool Equals(RussianDeclensionForms other)
-        {
-            if (ReferenceEquals(null, other)) return false;
-            if (ReferenceEquals(this, other)) return true;
-            return string.Equals(this.Nominative, other.Nominative) && string.Equals(this.Genitive, other.Genitive)
-                   && string.Equals(this.Dative, other.Dative) && string.Equals(this.Accusative, other.Accusative)
-                   && string.Equals(this.Instrumental, other.Instrumental)
-                   && string.Equals(this.Prepositional, other.Prepositional)
-                   && string.Equals(this.PrepositionalWithO, other.PrepositionalWithO);
-        }
-
-        [SuppressMessage("ReSharper", "StyleCop.SA1503")]
-        [SuppressMessage("ReSharper", "StyleCop.SA1126")]
-        [SuppressMessage("ReSharper", "ArrangeThisQualifier")]
-        public override bool Equals(object obj)
-        {
-            if (ReferenceEquals(null, obj)) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != this.GetType()) return false;
-            return this.Equals((RussianDeclensionForms)obj);
-        }
-
-        [SuppressMessage("ReSharper", "StyleCop.SA1119")]
-        [SuppressMessage("ReSharper", "NonReadonlyMemberInGetHashCode")]
-        public override int GetHashCode()
-        {
-            unchecked
-            {
-                var hashCode = Nominative?.GetHashCode() ?? 0;
-                hashCode = (hashCode * 397) ^ (Genitive?.GetHashCode() ?? 0);
-                hashCode = (hashCode * 397) ^ (Dative?.GetHashCode() ?? 0);
-                hashCode = (hashCode * 397) ^ (Accusative?.GetHashCode() ?? 0);
-                hashCode = (hashCode * 397) ^ (Instrumental?.GetHashCode() ?? 0);
-                hashCode = (hashCode * 397) ^ (Prepositional?.GetHashCode() ?? 0);
-                hashCode = (hashCode * 397) ^ (PrepositionalWithO?.GetHashCode() ?? 0);
-                return hashCode;
-            }
-        }
-    }
+   }
 }
