76e238ab0612ad3955443ff90d511a013dad1bce
Sergey Slepov <slepov@gmail.com>
2017-05-19 14:19:51 +0100

    Отформатирован и расширен код примера вызова веб-сервиса.

--- Morpher.WebService.V3.Client.Samples/Program.cs
+++ Morpher.WebService.V3.Client.Samples/Program.cs
@@ -16,58 +16,83 @@
             // string url = "http://api3.morpher.ru"
             // IMorpherClient morpherClient = new MorpherClient(token, url);
             IMorpherClient morpherClient = new MorpherClient();
+            const string premium = "*****";
 
-            // Склонение русских слов.
-            Console.WriteLine("Склонение русских слов:");
+            Console.WriteLine("Склонение на русском языке:");
             RussianDeclensionResult russianDeclensionResult = morpherClient.Russian.Parse("Соединенное королевство");
-            Console.WriteLine($"Именительный падеж: {russianDeclensionResult.Nominative},"
-                + $" Родительный падеж: {russianDeclensionResult.Genitive}\n");
+            Console.WriteLine("Именительный падеж: {0}", russianDeclensionResult.Nominative);
+            Console.WriteLine(" Родительный падеж: {0}", russianDeclensionResult.Genitive);
+            Console.WriteLine("   Дательный падеж: {0}", russianDeclensionResult.Dative);
+            Console.WriteLine(" Винительный падеж: {0}", russianDeclensionResult.Accusative);
+            Console.WriteLine("Творительный падеж: {0}", russianDeclensionResult.Instrumental);
+            Console.WriteLine("  Предложный падеж: {0}", russianDeclensionResult.Prepositional);
+            Console.WriteLine("     Местный падеж: {0}", russianDeclensionResult.Locative ?? premium);
+            Console.WriteLine("               Где? {0}", russianDeclensionResult.Where    ?? premium);
+            Console.WriteLine("              Куда? {0}", russianDeclensionResult.To       ?? premium);
+            Console.WriteLine("            Откуда? {0}", russianDeclensionResult.From     ?? premium);
+            if (russianDeclensionResult.Plural != null)
+            {
+                Console.WriteLine("Именительный падеж: {0}", russianDeclensionResult.Plural.Nominative);
+                Console.WriteLine(" Родительный падеж: {0}", russianDeclensionResult.Plural.Genitive);
+                Console.WriteLine("   Дательный падеж: {0}", russianDeclensionResult.Plural.Dative);
+                Console.WriteLine(" Винительный падеж: {0}", russianDeclensionResult.Plural.Accusative);
+                Console.WriteLine("Творительный падеж: {0}", russianDeclensionResult.Plural.Instrumental);
+                Console.WriteLine("  Предложный падеж: {0}", russianDeclensionResult.Plural.Prepositional);
+                Console.WriteLine("     Местный падеж: {0}", russianDeclensionResult.Plural.Locative ?? premium);
+            }
+            Console.WriteLine();
 
             // Поле род доступно только на платных тарифах. Подробнее http://morpher.ru/WebServiceV3.aspx
-            Console.WriteLine("Род (только на платных тарифах):");
-            Console.WriteLine($"Род: {russianDeclensionResult.Gender}");
+            Console.WriteLine("Определение рода на русском языке:");
+            Console.WriteLine("Род: {0}", russianDeclensionResult.Gender ?? premium);
+            Console.WriteLine();
 
-            // Передача признаков для улучшения качества склонения, а так же разделение ФИО на части
-            Console.WriteLine("Разделение на ФИО:");
-            RussianDeclensionResult nameDeclensionResult =
-                morpherClient.Russian.Parse("Крутько Екатерина Володимировна", DeclensionFlags.Name);
-            Console.WriteLine($"Ф: {nameDeclensionResult.FullName.Surname} "
-                + $"И: {nameDeclensionResult.FullName.Name} " +
-                $"О: {nameDeclensionResult.FullName.Pantronymic}\n");
+            Console.WriteLine("Разделение ФИО на части:");
+            RussianDeclensionResult nameDeclensionResult = morpherClient.Russian.Parse("Полад Бюльбюль-оглы Мамедов");
+            Console.WriteLine("Ф: " + nameDeclensionResult.FullName.Surname);
+            Console.WriteLine("И: " + nameDeclensionResult.FullName.Name);
+            Console.WriteLine("О: " + nameDeclensionResult.FullName.Pantronymic);
+            Console.WriteLine();
 
-            // Склонение украинских ФИО
-            Console.WriteLine("Склонения украинских ФИО:");
+            Console.WriteLine("Склонение ФИО на украинском языке:");
             UkrainianDeclensionResult ukrainianDeclensionResult = morpherClient.Ukrainian.Parse("Крутько Катерина Володимирiвна");
-            Console.WriteLine($"Називний вiдмiнок: {ukrainianDeclensionResult.Nominative}," +
-                $" Родовий вiдмiнок: {ukrainianDeclensionResult.Genitive}\n");
+            Console.WriteLine(" Називний вiдмiнок: " + ukrainianDeclensionResult.Nominative);
+            Console.WriteLine("  Родовий вiдмiнок: " + ukrainianDeclensionResult.Genitive);
+            Console.WriteLine("Давальний вiдмiнок: " + ukrainianDeclensionResult.Dative);
+            Console.WriteLine("Знахідний вiдмiнок: " + ukrainianDeclensionResult.Accusative);
+            Console.WriteLine("  Орудний вiдмiнок: " + ukrainianDeclensionResult.Instrumental);
+            Console.WriteLine(" Місцевий вiдмiнок: " + ukrainianDeclensionResult.Prepositional);
+            Console.WriteLine("  Кличний вiдмiнок: " + ukrainianDeclensionResult.Vocative);
+            Console.WriteLine();
 
-            // Сумма прописью
+            Console.WriteLine("Определение рода на украинском языке:");
+            Console.WriteLine("Род: {0}", ukrainianDeclensionResult.Gender ?? premium);
+            Console.WriteLine();
+
             Console.WriteLine("Сумма прописью на двух языках:");
             uint number = 2513;
-            RussianNumberSpellingResult russianNumberSpellingResult =
-                morpherClient.Russian.Spell(number, "рубль");
-            Console.WriteLine($"В размере {number} ({russianNumberSpellingResult.NumberDeclension.Genitive}) " 
-                + $"{russianNumberSpellingResult.UnitDeclension.Genitive}");
+            RussianNumberSpellingResult russianNumberSpellingResult = morpherClient.Russian.Spell(number, "рубль");
+            Console.WriteLine("В размере {0} ({1}) {2}", number,
+                    russianNumberSpellingResult.NumberDeclension.Genitive,
+                    russianNumberSpellingResult.UnitDeclension.Genitive);
             UkrainianNumberSpellingResult ukrainianNumberSpellingResult = morpherClient.Ukrainian.Spell(number, "рубль");
-            Console.WriteLine($"У розмірі {number} ({ukrainianNumberSpellingResult.NumberDeclension.Genitive}) " 
-                + $"{ukrainianNumberSpellingResult.UnitDeclension.Genitive}\n");
+            Console.WriteLine("У розмірі {0} ({1}) {2}", number,
+                    ukrainianNumberSpellingResult.NumberDeclension.Genitive,
+                    ukrainianNumberSpellingResult.UnitDeclension.Genitive);
+            Console.WriteLine();
 
-            // Склонение прилагательных по родам
             Console.WriteLine("Склонение прилагательных по родам:");
             AdjectiveGenders adjectiveGenders = morpherClient.Russian.AdjectiveGenders("уважаемый");
-            Console.WriteLine($"Женский: {adjectiveGenders.Feminie}\n" 
-                + $"Средний: {adjectiveGenders.Neuter}\n" + 
-                $"Множественное число: {adjectiveGenders.Plural}\n");
+            Console.WriteLine("Женский род:         " + adjectiveGenders.Feminie);
+            Console.WriteLine("Средний род:         " + adjectiveGenders.Neuter);
+            Console.WriteLine("Множественное число: " + adjectiveGenders.Plural);
+            Console.WriteLine();
 
-            // Образования прилагательных
             Console.WriteLine("Образование прилагательных:");
             List<string> adjectives = morpherClient.Russian.Adjectivize("Мытищи");
             adjectives.ForEach(Console.WriteLine);
+            Console.WriteLine();
 
-            // Остаток запросов
-            Console.WriteLine($"\nОстаток запросов: {morpherClient.QueriesLeftForToday()}\n");
-
-            // Обработка ошибок сервиса
             Console.WriteLine("Обработка ошибок сервиса:");
             try
             {
@@ -75,8 +100,14 @@
             }
             catch (MorpherWebServiceException exc)
             {
-                Console.WriteLine($"Code: {exc.Code} Message: {exc.Message}");
+                Console.WriteLine("Code: {0} Message: {1}", exc.Code, exc.Message);
             }
+            Console.WriteLine();
+
+            Console.WriteLine("Остаток запросов на сегодня: " + morpherClient.QueriesLeftForToday());
+            Console.WriteLine();
+
+            Console.WriteLine(premium + " означает, что функция доступна на платных тарифах.");
         }
     }
 }
