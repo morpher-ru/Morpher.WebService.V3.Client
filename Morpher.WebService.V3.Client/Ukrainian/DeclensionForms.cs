89d90899d1a3e9a720acb8b3ac97af16ab1de0e5
Sergey Slepov <slepov@gmail.com>
2017-05-19 20:56:12 +0100

    Format code in Ukrainian/DeclensionForms.cs.

--- Morpher.WebService.V3.Client/Ukrainian/DeclensionForms.cs
+++ Morpher.WebService.V3.Client/Ukrainian/DeclensionForms.cs
@@ -5,25 +5,12 @@ namespace Morpher.WebService.V3.Ukrainian
     [DataContract]
     public class DeclensionForms
     {
-        [DataMember(Name = "Н")]
-        public string Nominative { get; set; }
-
-        [DataMember(Name = "Р")]
-        public string Genitive { get; set; }
-
-        [DataMember(Name = "Д")]
-        public string Dative { get; set; }
-
-        [DataMember(Name = "З")]
-        public string Accusative { get; set; }
-
-        [DataMember(Name = "О")]
-        public string Instrumental { get; set; }
-
-        [DataMember(Name = "М")]
-        public string Prepositional { get; set; }
-
-        [DataMember(Name = "К")]
-        public string Vocative { get; set; }
+        [DataMember(Name = "Н")] public string Nominative { get; set; }
+        [DataMember(Name = "Р")] public string Genitive { get; set; }
+        [DataMember(Name = "Д")] public string Dative { get; set; }
+        [DataMember(Name = "З")] public string Accusative { get; set; }
+        [DataMember(Name = "О")] public string Instrumental { get; set; }
+        [DataMember(Name = "М")] public string Prepositional { get; set; }
+        [DataMember(Name = "К")] public string Vocative { get; set; }
     }
 }
