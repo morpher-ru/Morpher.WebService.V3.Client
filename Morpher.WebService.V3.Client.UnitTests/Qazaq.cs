07f952919757acf8e5fda882638459ea90a796f9
my_official <my_official@outlook.com>
2018-12-01 01:26:23 +0800

    Поправил ошибку с названием казахского именительного падежа в xml. Добавил тест

---
+++ Morpher.WebService.V3.Client.UnitTests/Qazaq.cs
@@ -0,0 +1,58 @@
+﻿namespace Morpher.WebService.V3.Client.UnitTests
+{    
+    using System.Collections.Specialized;
+    using Moq;
+    using NUnit.Framework;
+    using V3.Qazaq;
+
+    [TestFixture]
+    class Qazaq
+    {
+        string DeclensionResultText { get; } = @"
+{
+    ""І"": ""тесттің"",
+    ""Б"": ""тестке"",
+    ""Т"": ""тестті"",
+    ""Ш"": ""тесттен"",
+    ""Ж"": ""тестте"",
+    ""К"": ""тестпен"",    
+    ""көпше"": {
+        ""A"": ""тесттер"",
+        ""І"": ""тесттертің"",
+        ""Б"": ""тесттерке"",
+        ""Т"": ""тесттерті"",
+        ""Ш"": ""тесттертен"",
+        ""Ж"": ""тесттерте"",
+        ""К"": ""тесттерпен""
+    }
+}";
+
+        [Test]
+        public void Parse_Success()
+        {
+            var webClient = new Mock<IWebClient>();
+            webClient.Setup(client => client.QueryString).Returns(new NameValueCollection());
+            webClient.Setup(client => client.DownloadString(It.IsAny<string>())).Returns(DeclensionResultText);
+            var morpherClient = new MorpherClient(null, null, webClient.Object);
+
+            DeclensionResult declensionResult = morpherClient.Qazaq.Parse("тест");
+            Assert.IsNotNull(declensionResult);
+            Assert.IsNotNull(declensionResult.Plural);
+            Assert.AreEqual("тест", declensionResult.Nominative);
+            Assert.AreEqual("тесттің", declensionResult.Genitive);
+            Assert.AreEqual("тестке", declensionResult.Dative);
+            Assert.AreEqual("тестті", declensionResult.Accusative);
+            Assert.AreEqual("тесттен", declensionResult.Instrumental);
+            Assert.AreEqual("тестте", declensionResult.Prepositional);
+            Assert.AreEqual("тестпен", declensionResult.Vocative);
+
+            Assert.AreEqual("тесттер", declensionResult.Plural.Nominative);
+            Assert.AreEqual("тесттертің", declensionResult.Plural.Genitive);
+            Assert.AreEqual("тесттерке", declensionResult.Plural.Dative);
+            Assert.AreEqual("тесттерті", declensionResult.Plural.Accusative);
+            Assert.AreEqual("тесттертен", declensionResult.Plural.Instrumental);
+            Assert.AreEqual("тесттерте", declensionResult.Plural.Prepositional);
+            Assert.AreEqual("тесттерпен", declensionResult.Plural.Vocative);
+        }
+    }
+}
--- Morpher.WebService.V3.Client/Qazaq/DeclensionForms.cs
+++ Morpher.WebService.V3.Client/Qazaq/DeclensionForms.cs
@@ -5,7 +5,7 @@ namespace Morpher.WebService.V3.Qazaq
     [DataContract]
     public class DeclensionForms
     {
-        [DataMember(Name = "А")] public string Nominative { get; set; }
+        [DataMember(Name = "A")] public string Nominative { get; set; }
         [DataMember(Name = "І")] public string Genitive { get; set; }
         [DataMember(Name = "Б")] public string Dative { get; set; }
         [DataMember(Name = "Т")] public string Accusative { get; set; }
