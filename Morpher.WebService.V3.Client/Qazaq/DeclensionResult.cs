a8aacc95da0c4c1c3a3e4a1dbb9598ce5c780a1d
Sergey Slepov <slepov@gmail.com>
2019-06-03 18:00:41 +0300

    Rename document XML node to QazaqDeclensionResult.

--- Morpher.WebService.V3.Client/Qazaq/DeclensionResult.cs
+++ Morpher.WebService.V3.Client/Qazaq/DeclensionResult.cs
@@ -2,7 +2,7 @@
 
 namespace Morpher.WebService.V3.Qazaq
 {
-    [DataContract(Name = "qazaqXml", Namespace = "http://schemas.datacontract.org/2004/07/Morpher.WebApi.Models")]
+    [DataContract(Name = "QazaqDeclensionResult", Namespace = "http://schemas.datacontract.org/2004/07/Morpher.WebApi.Models")]
     public class DeclensionResult : DeclensionForms
     {
         [DataMember(Name = "көпше")]
