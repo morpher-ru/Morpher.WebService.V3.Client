ca1a123cf7c52b66a24de37a73676345ca589e6b
Sergey Slepov <slepov@gmail.com>
2019-06-03 18:12:56 +0300

    Забыл сохранить файл.

--- Morpher.WebService.V3.Client/Qazaq/DeclensionResult.cs
+++ Morpher.WebService.V3.Client/Qazaq/DeclensionResult.cs
@@ -2,36 +2,6 @@
 
 namespace Morpher.WebService.V3.Qazaq
 {
-    /// <summary>
-    /// Падежно-личные формы одного числа.
-    /// </summary>
-    public class SameNumberForms : DeclensionForms
-    {
-        [DataMember(Name = "менің")]
-        public DeclensionForms FirstPerson { get; set; }
-
-        [DataMember(Name = "cенің")]
-        public DeclensionForms SecondPerson { get; set; }
-
-        [DataMember(Name = "сіздің")]
-        public DeclensionForms SecondPersonRespectful { get; set; }
-
-        [DataMember(Name = "оның")]
-        public DeclensionForms ThirdPerson { get; set; }
-
-        [DataMember(Name = "біздің")]
-        public DeclensionForms FirstPersonPlural { get; set; }
-
-        [DataMember(Name = "cендердің")]
-        public DeclensionForms SecondPersonPlural { get; set; }
-
-        [DataMember(Name = "сіздердің")]
-        public DeclensionForms SecondPersonRespectfulPlural { get; set; }
-
-        [DataMember(Name = "олардың")]
-        public DeclensionForms ThirdPersonPlural { get; set; }
-    }
-
     [DataContract(Name = "QazaqDeclensionResult", Namespace = "http://schemas.datacontract.org/2004/07/Morpher.WebApi.Models")]
     public class DeclensionResult : SameNumberForms
     {
